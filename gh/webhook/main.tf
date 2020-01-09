aedbbc76162c38b0b8288acb43cdf8b27c317d90
Graham Burgess <stormmore@gmail.com>
2020-01-09 12:32:42 -0800

    Switched to s3 state backend

--- gh/webhook/main.tf
+++ gh/webhook/main.tf
@@ -9,20 +9,20 @@ resource "random_id" "webhook" {
 }
 
 resource "aws_ssm_parameter" "webhook" {
-  name = "/${lower(replace(var.name, "_", "/"))}"
-  type = "SecureString"
+  name  = "/${lower(replace(var.name, "_", "/"))}"
+  type  = "SecureString"
   value = random_id.webhook.hex
 }
 
 module "webhooks" {
-  source              = "git::https://github.com/cloudposse/terraform-github-repository-webhooks.git?ref=tags/0.5.0"
+  source = "git::https://github.com/cloudposse/terraform-github-repository-webhooks.git?ref=tags/0.5.0"
 
-  active              = false
+  active              = var.active
   github_token        = var.github_token
   github_organization = var.github_organization
   github_repositories = var.github_repositories
 
-  webhook_secret      = random_id.webhook.hex
-  webhook_url         = var.webhook_url
-  events              = var.events
+  webhook_secret = random_id.webhook.hex
+  webhook_url    = var.webhook_url
+  events         = var.events
 }
